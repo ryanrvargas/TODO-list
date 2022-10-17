78bfef4a9ccc3972d547c7b0085fea371c3854cc
Ryan Vargas <ryanr99@live.com>
2022-10-17 10:57:07 -0400

    Update toDO.py

--- toDO.py
+++ toDO.py
@@ -2,9 +2,11 @@ list = []
 
 user = input("How many Task are in your list ")
 
+####get task from user
 for x in range(int(user)):
     user = input("Task:" + str(x + 1) + " ")
-    list += [user]
-    
+    list += [user] #makes list array size
+
+####output tasks from user
 for x in range(len(list)):
     print(list[x])
