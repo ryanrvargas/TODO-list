42ed7f6ee703502633fef9c8fb0c404cc8f6105e
Ryan Vargas <ryanr99@live.com>
2022-10-17 13:48:22 -0400

    toDo file updated

--- toDO.py
+++ toDO.py
@@ -1,12 +1,44 @@
-list = []
-
-user = input("How many Task are in your list ")
+list = [""]
 
 ####get task from user
-for x in range(int(user)):
-    user = input("Task:" + str(x + 1) + " ")
-    list += [user] #makes list array size
+def getTask(task):
+    global list
+    task = []
+    user = input("How many Task are in your list ")
+    ####Make sure user inputs a integer
+    try:
+        for x in range(int(user)):
+            user = input("Task:" + str(x + 1) + " ")
+            task += [user] #makes list array size
+        pass
+    except ValueError:
+        print("That was not an integer please try again")
+    
+    for x in range(len(task)):
+        #print(x, end = " ")
+        list += [task]
+        list[x] = task[x]
+        
+    
+    
+####removing task from 
+def removeTask():
+    word = input("What task would you like to remove ")
+    """
+    for x in range(len(list)):
+        if word.lower() == list[x]:
+            list[x] = ""
+            """
 
 ####output tasks from user
-for x in range(len(list)):
-    print(list[x])
+def printTask():
+    print("-----Current Task-----")
+    for x in range(len(list)):
+        print(list[x])
+
+####get task from user
+getTask(list)
+printTask()
+removeTask()
+printTask()
+    
